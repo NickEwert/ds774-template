7b46791f72613bc6993c9f856f7dbc9662df81ad
Nick Ewert <nickewert@gmail.com>
2022-03-21 19:19:57 -0400

    test

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.secret_key = "IAN"
 def home():
     return render_template('home.html')
 
-@app.route("/courses")
+@app.route("/documents")
 def courses():
     return render_template('courses.html')
 
